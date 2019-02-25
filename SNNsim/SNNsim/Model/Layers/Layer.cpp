d045b1ee1271f8fee83b895ad200fa330c372fe1
RyanNavillus <ryanpnavillus@gmail.com>
2019-02-24 22:34:01 -0500

    Starting Layer interface

---
+++ SNNsim/SNNsim/Model/Layers/Layer.cpp
@@ -0,0 +1,10 @@
+//
+//  Layer.cpp
+//  SNNsim
+//
+//  Created by Ryan Sullivan on 2/24/19.
+//  Copyright © 2019 Ryan Sullivan. All rights reserved.
+//
+
+#include "Layer.hpp"
+
---
+++ SNNsim/SNNsim/Model/Layers/Layer.hpp
@@ -0,0 +1,18 @@
+//
+//  Layer.hpp
+//  SNNsim
+//
+//  Created by Ryan Sullivan on 2/24/19.
+//  Copyright © 2019 Ryan Sullivan. All rights reserved.
+//
+
+#ifndef Layer_hpp
+#define Layer_hpp
+
+#include <stdio.h>
+
+class Layer {
+    
+};
+
+#endif /* Layer_hpp */
